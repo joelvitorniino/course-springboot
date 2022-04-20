fdcf22cb92abadd2455538a1c56836ed878c2157
joelvitorniino <joelprofessor13@gmail.com>
2022-04-20 16:17:09 -0300

    Order, Instant, ISO 8601 - part 2

--- src/main/java/com/educandoweb/workshop/entities/Order.java
+++ src/main/java/com/educandoweb/workshop/entities/Order.java
@@ -1,5 +1,7 @@
 package com.educandoweb.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -11,6 +13,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
