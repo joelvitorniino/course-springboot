4c21b188171494f5434ff0854d3cd4ec803d7871
= <joelprofessor13@gmail.com>
2022-04-28 23:52:28 -0300

    User insert

--- src/main/java/com/educandoweb/workshop/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshop/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +26,14 @@ public class UserResource {
         User obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(obj);
     }
 }
