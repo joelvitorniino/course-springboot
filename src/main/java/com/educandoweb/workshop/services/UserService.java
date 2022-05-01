93a69b7f6082560cec51a9c070afcc63449bec56
= <joelprofessor13@gmail.com>
2022-05-01 19:20:14 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/workshop/services/UserService.java
+++ src/main/java/com/educandoweb/workshop/services/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.workshop.services;
 
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.repositories.UserRepository;
+import com.educandoweb.workshop.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
 
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/educandoweb/workshop/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.educandoweb.workshop.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found: Id " + id);
+    }
+}
